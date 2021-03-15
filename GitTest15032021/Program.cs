4e20c4221cb180bdc9acd9c89636b897f3fdda17
frunt <frunt@DESKTOP-8L021IF>
2021-03-15 17:54:57 +0200

    Добавьте файлы проекта.

---
+++ GitTest15032021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTest15032021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
